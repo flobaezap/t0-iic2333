629bcbb528d660aa084dafb7a55f10fc5c4eb947
flobaezap <florencia.baeza@uc.cl>
2026-03-18 20:22:53 -0300

    Archivos Base

---
+++ src/Burnssh/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../input_manager/manager.h"
+
+int main(int argc, char const *argv[])
+{
+  set_buffer(); // No borrar
+
+  char** input = read_user_input();
+  printf("%s\n", input[0]);
+  free_user_input(input);
+}
+
